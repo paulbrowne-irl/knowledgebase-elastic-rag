9dd712ec03df886b899ebf6428e38505033f4392
Paul Browne <1431190+paulbrowne-irl@users.noreply.github.com>
2024-05-04 21:19:13 +0100

    class tidy

--- app/util_nlp/_redo_calc_sentiment.py
+++ app/util_nlp/_redo_calc_sentiment.py
@@ -19,6 +19,27 @@ import util.file_export as file_export
 import util.file_util as file_util
 
 
+from dataclasses import dataclass,field
+
+from typing import Dict
+
+@dataclass
+class company_data():
+    """This is a data holder for the information we extract on a company"""
+
+    #key_information_to_extract
+    key_info: Dict[str, str] = field(default_factory = lambda: ({}))
+
+    # tables(sheets) that we pull from pdf, and the (sheet) names we have identifed for them
+    tables:  list = field(default_factory=lambda : [] , repr=False) #repr means it won't get printed out
+    table_names:  list = field(default_factory=lambda : [])
+
+    #working information - not as important
+    original_pdf_name: str =""
+
+    #nltk info and keywords
+    keyword_info: list = field(default_factory=lambda : [])
+
 
 def _extract_text_stats(source_file_name,df_to_append_to,document):
     '''
