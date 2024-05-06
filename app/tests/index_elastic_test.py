ad8b5928fa0e48892f934f6ff6728e872c68eaa6
Paul Browne <1431190+paulbrowne-irl@users.noreply.github.com>
2024-05-06 20:13:01 +0100

    ingest unit test passing

--- app/tests/index_elastic_test.py
+++ app/tests/index_elastic_test.py
@@ -28,14 +28,14 @@ class Test_Index(unittest.TestCase):
         }
 
         #try call
-        index_elastic.index(index_name= "test",filepath="file.pdf" ,filecontents="",meta_data=test_meta)
+        index_elastic.index(index_name= "test-can-del",filepath="../data-sample/ingest/sample_test_outlook_email.msg" ,filecontents="blah blah blah",meta_data=test_meta)
         # no exception is success - update?
 
 
 
     def test_index_pdf(self):
 
-        index_elastic.index(index_name= "test",filepath="file.msg" ,filecontents="XYZ")
+        index_elastic.index(index_name= "test-can-del",filepath="../data-sample/ingest/companyprofile.pdf" ,filecontents="doh ray me fah so la tee doh")
         # no exception is success - update?
 
 
