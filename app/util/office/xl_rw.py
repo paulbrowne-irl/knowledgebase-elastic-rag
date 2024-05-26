9add821e0b1d56a823421dd48bffbe7ec283febf
Paul Browne <1431190+paulbrowne-irl@users.noreply.github.com>
2024-05-26 21:59:35 +0100

    stub of bot loop

--- app/util/office/xl_rw.py
+++ app/util/office/xl_rw.py
@@ -6,7 +6,7 @@ import pandas as pd
 '''
 Util file to read and write excel file
 '''
-
+'''
 def has_unaswered_question(xl_file_name:str,answer_col:str)->bool:
 
         
@@ -19,15 +19,13 @@ def has_unaswered_question(xl_file_name:str,answer_col:str)->bool:
     # filter based on answered question
     filtered_table = question_table[question_table[answer_col].isnull()]
 
-    #close file
-
     # return 
     return len(filtered_table.index)>0
+'''
 
 
 
-
-def read_next_unanswered_question(xl_file_name:str,question_col:str,answer_col:str)->pd.DataFrame:
+def read_unanswered_questions(xl_file_name:str,question_col:str,answer_col:str)->pd.DataFrame:
 
     '''read values from xl file
     Keyword Arguments:
@@ -39,7 +37,7 @@ def read_next_unanswered_question(xl_file_name:str,question_col:str,answer_col:s
     
     #root_folder = .Folders.Item(1)
     logging.debug("Getting handle to Excel with Emails");
-    question_table = pd.read_excel(xl_file_name, index_col=0)
+    question_table = pd.read_excel(xl_file_name)
 
     logging.info("Filtering based on unanswered questions in:"+answer_col) 
 
@@ -47,5 +45,5 @@ def read_next_unanswered_question(xl_file_name:str,question_col:str,answer_col:s
     filtered_table = question_table[question_table[answer_col].isnull()]
 
     #return only the first row of this filtered table
-    return filtered_table.iloc[0]
+    return filtered_table
 
