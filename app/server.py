270822a2ca97e5e90d42b7cd231167601ae804a5
Paul Browne <1431190+paulbrowne-irl@users.noreply.github.com>
2024-10-14 18:32:17 +0100

    server code tidy

--- app/server.py
+++ app/server.py
@@ -3,22 +3,25 @@ from langserve import add_routes
 
 from lang_server.chain_factory import get_chain
 
+import uvicorn
 import logging
 
 
-'''
-Lang Serve server which makes Lang chain available via REST API
-When run normally, it is available via http://localhost:8001/docs
-'''
 
-app = FastAPI(title="LangServe Knowledbase Example")
+def start():
 
-add_routes(app, get_chain())
+    '''
+    Lang Serve server which makes Lang chain available via REST API
+    When run normally, it is available via http://localhost:8001/docs
+    '''
+
+    app = FastAPI(title="LangServe Knowledbase Example")
+    add_routes(app, get_chain())
+
+    logging.info("Starting LangServe on http://localhost:8001")
+    uvicorn.run(app, host="0.0.0.0", port=8001)
 
 
 # when called via command line
 if __name__ == "__main__":
-    import uvicorn
-
-    logging.info("Started LangServe on http://localhost:8001")
-    uvicorn.run(app, host="0.0.0.0", port=8001)
+    start()    
